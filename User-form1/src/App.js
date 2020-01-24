8f242bb4535de0fdec14902f4f64780962304704
Victor Sanchez <v08311996@gmail.com>
2020-01-24 14:08:44 -0500

    MVP

---
+++ User-form1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import MyForm from "./Form"
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <MyForm/>
+    </div>
+  );
+}
+
+export default App;
